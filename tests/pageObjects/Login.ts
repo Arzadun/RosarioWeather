7b480180cc59dc1387d734457db9b6beb05c11a5
Sebastian <sebastianarzadun@gmail.com>
2024-04-29 22:57:11 -0300

    Login functionality

---
+++ tests/pageObjects/Login.ts
@@ -0,0 +1,52 @@
+import { BasePage } from "./BasePage";
+import credentials from '../../credentials.json'
+
+export class Login extends BasePage{
+
+private readonly loginButton;
+private readonly signInButton;
+private readonly inputUsername;
+private readonly inputPassword;
+private readonly nextButton;
+
+
+
+
+constructor(page){
+    super(page);
+
+    
+    this.loginButton = ('//span[text()[contains(.,\'Log in\')]]/ancestor::div[1]');
+   this.inputUsername = ('//input[@class="r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7"]') //"#login-username"
+   this.inputPassword = ('//input[@type=\'password\']') 
+   this.signInButton = ('//span[text()[contains(.,\'Sign in\')]]/ancestor::div[1]') 
+    this.nextButton = ('//span[text()[contains(.,\'Next\')]]/ancestor::div[1]') 
+    
+}
+/*
+async searchLocation(location:string) {
+    this.fillInput(this.searchInput, location);
+    this.clickElement(this.searchResult);
+}
+
+async openLoginModal(){
+    this.clickElement(this.loginButton);
+} */
+
+//CAMBIAR XPATHS
+async loginIntoAccount(){
+
+    this.clickElement(this.signInButton);
+    this.fillInput(this.inputUsername, credentials.username);
+    this.clickElement(this.nextButton);
+    this.fillInput(this.inputPassword, credentials.password);
+    this.clickElement(this.loginButton);
+}
+
+
+
+
+}
+
+
+
---
+++ tests/weatherRosario.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+import { Login } from './pageObjects/login';
+
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+})
+
+test('get started link', async ({ page, request }) => {
+
+
+  const response = await request.get("https://api.open-meteo.com/v1/forecast?latitude=-32.9468&longitude=-60.6393&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto&forecast_days=1")
+
+  expect(response.status()).toBe(200);
+  
+  let responseBody = await response.json();
+
+  let maxTemperature = `${responseBody.daily.temperature_2m_max[0]} ${responseBody.daily_units.temperature_2m_max}`;
+  let minTemperature = `${responseBody.daily.temperature_2m_min[0]} ${responseBody.daily_units.temperature_2m_min}`;
+  let sunrise = responseBody.daily.sunrise[0].split("T")[1];
+  let sunset = responseBody.daily.sunset[0].split("T")[1];
+ 
+
+  const login = new Login(page);
+
+  await page.goto('https://twitter.com/i/flow/login');
+  //await page.waitForNavigation();
+  
+  await login.loginIntoAccount();
+  
+  await page.pause();
+  
+  
+
+  
+
+
+  //TODO: add wheater code to parameter and add that info weather_code
+  /*
+
+WMO Weather interpretation codes (WW)
+Code	Description
+0	Clear sky
+1, 2, 3	Mainly clear, partly cloudy, and overcast
+45, 48	Fog and depositing rime fog
+51, 53, 55	Drizzle: Light, moderate, and dense intensity
+56, 57	Freezing Drizzle: Light and dense intensity
+61, 63, 65	Rain: Slight, moderate and heavy intensity
+66, 67	Freezing Rain: Light and heavy intensity
+71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+77	Snow grains
+80, 81, 82	Rain showers: Slight, moderate, and violent
+85, 86	Snow showers slight and heavy
+95 *	Thunderstorm: Slight or moderate
+96, 99 *	Thunderstorm with slight and heavy hail
+(*) Thunderstorm forecast with hail is only available in Central Europe
+  */
+  
+  // Click the get started link.
+ // await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+ // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
