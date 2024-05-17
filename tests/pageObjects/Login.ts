b6c9f2b52e1df63fdd472572717624a2f244eecc
Sebastian <sebastianarzadun@gmail.com>
2024-05-16 22:46:26 -0300

    updated security check login to handle code

--- tests/pageObjects/Login.ts
+++ tests/pageObjects/Login.ts
@@ -11,20 +11,23 @@ export class Login extends BasePage{
     private readonly inputPassword: string;
     private readonly nextButton: string;
     private readonly sidePostButton: string;
-    private readonly inputCheck: string;
+    private readonly inputCheckEmail: string;
     private readonly email: string;
-
+    private readonly inputCheckCode: string;
 
 constructor(page){
     super(page);
     this.loginButton = ('//span[text()[contains(.,\'Log in\')]]/ancestor::div[1]');
    this.inputUsername = ('//input[@class="r-30o5oe r-1dz5y72 r-13qz1uu r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-t60dpp r-fdjqy7"]') //"#login-username"
-   this.inputPassword = ('//input[@type=\'password\']') 
-   this.signInButton = ('//span[text()[contains(.,\'Sign in\')]]/ancestor::div[1]') 
-    this.nextButton = ('//span[text()[contains(.,\'Next\')]]/ancestor::div[1]') 
+   this.inputPassword = ('//input[@type=\'password\']'); 
+   this.signInButton = ('//span[text()[contains(.,\'Sign in\')]]/ancestor::div[1]'); 
+    this.nextButton = ('//span[text()[contains(.,\'Next\')]]/ancestor::div[1]'); 
     this.sidePostButton = ("//a[@data-testid='SideNav_NewTweet_Button']");
-    this.inputCheck =('//input[@type=\'email\']')
+    this.inputCheckEmail =('//input[@type=\'email\']')
     this.email = "weatherrosario@mailinator.com";
+    this.inputCheckCode =("//input[@data-testid='ocfEnterTextTextInput']")
+    
+
     
 }
 /*
@@ -50,17 +53,25 @@ async validateUserIsLoggedIn(){
     await this.validateElementIsVisible(this.sidePostButton);
 }
 
-async bypassCheck (){
-    await this.fillInput(this.inputCheck, this.email);
+async bypassCheck(){
+    if (await this.validateElementIsVisible(this.inputCheckEmail)) {
+        await this.fillInput(this.inputCheckEmail, this.email);
+    } else if (await this.validateElementIsVisible(this.inputCheckCode)) {
+        await this.fillInput(this.inputCheckCode, this.email);
+    }
     await this.clickElement(this.nextButton);
 }
 
-async securityCheckIsDisplayed(): Promise<boolean>{
-    if(await this.validateElementIsVisible(this.inputCheck)){
+
+
+async securityCheckIsDisplayed(): Promise<boolean> {
+    if (await this.validateElementIsVisible(this.inputCheckEmail) || await this.validateElementIsVisible(this.inputCheckCode)) {
         return true;
     }
     return false;
 }
+
+
 
 }
 
